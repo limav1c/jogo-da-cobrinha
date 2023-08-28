7311cfbdeefb3756de7eb0ebc5edae19dccd1b56
limav1c <103318987+limav1c@users.noreply.github.com>
2023-08-28 14:16:12 -0300

    Update testar_classes_e_popular_tabelas.py

--- testar_classes_e_popular_tabelas.py
+++ testar_classes_e_popular_tabelas.py
@@ -1,10 +1,12 @@
-from cobrinha.config import *
-from cobrinha.modelo import *
+from config import *
+from modelo import *
 
 # cria um jogador
-jogador1 = Jogador(nome="Jack Good", macas_coletadas= Jogador.macas_coletadas)
+jogador1 = Jogador(id= 1, nome="Jack Good", macas_coletadas= 5)
+jogador2 = Jogador(id= 2, nome="Maria", macas_coletadas= 6)
 db.session.add(jogador1)
+db.session.add(jogador2)
 db.session.commit()
 
-print(jogador1)
+print(jogador1, jogador2)
 print("Dados inseridos")
